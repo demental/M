f22724f1765604364241adbf54ada7cf8ddf6ba3
raynald.coupe@easi-services.fr <raynald.coupe@easi-services.fr@4cff5740-f916-11dd-b507-8b4ecd5210be>
2009-02-24 17:16:22 +0000

    git-svn-id: https://m4php5.googlecode.com/svn/trunk@22 4cff5740-f916-11dd-b507-8b4ecd5210be

--- skeleton/config.domain_name.php
+++ skeleton/config.domain_name.php
@@ -1,12 +1,12 @@
 <?php
 
-// Project name - must be the name of the root folder (eg: project)
+// Change {$PROJECT_NAME} with your project name - must be the name of the project root folder (folder project in the skeleton)
 define('PROJECT_NAME','{$PROJECT_NAME}');
-// Site URL - http path to project (eg: http://localhost/www)
+// Change {$SITE_URL} with your site URL - http path to project (eg: http://localhost/my_project_folder)
 define('SITE_URL','{$SITE_URL}');
-// Web folder - relative to project (eg : www)
+// Change {$DOC_ROOT} with your Web folder - relative to project (eg : www)
 define('WEB_FOLDER','{$DOC_ROOT}');
-// Mode - production, development, test
+// Change {$MODE} with your mode -> eg: production or development or test
 define('MODE','{$MODE}');
 
 
@@ -15,8 +15,10 @@ define('MODE','{$MODE}');
 // =================================================
 // = Additional paths specific to host             =
 // =================================================
-{if $PEAR_PATH}
-$paths[]='{$PEAR_PATH}';
-{/if}
+
+// If PEAR path not in your PHP include path in your php.ini, uncomment this section and replace {$PEAR_PATH}
+/*
+paths[]='{$PEAR_PATH}';
+ */
 
 define('DB_URI','{$DB_URI}');
