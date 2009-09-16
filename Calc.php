a6ddfb201be7692930da295a6b3f061b3664c3af
arnodmental <arnodmental@4cff5740-f916-11dd-b507-8b4ecd5210be>
2009-09-16 16:15:08 +0000

    Small enhancement to Calc::HT2TTC
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@88 4cff5740-f916-11dd-b507-8b4ecd5210be

--- Calc.php
+++ Calc.php
@@ -35,7 +35,19 @@ class Calc
 	public static function HT2TTC($price,$vat)
 	{
 		$vat=$vat>1?$vat/100:$vat;
-		return number_format($price*(1+$vat),2, '.', '');
+   $result = $price*(1+$vat);
+   // We round the result to avoid 1cent difference
+   $mod = $result-floor($result);
+   if($mod==0) {
+     // no round
+   } elseif($result<0.02) {
+     $result = 0;
+   } elseif(($mod > 0.48 && $mod < 0.50) || ($mod > 0.98)) {
+     $result+=0.006;
+   } elseif(($mod > 0.50 && $mod < 0.52) || ($mod < 0.02)) {
+     $result-=0.006;
+   }
+		return number_format($result,2, '.', '');
 	}
 	// Alias
   public static function addVAT($price,$vat)
