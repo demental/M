eb5e015447a0d9b590336298c1a76e40f2d738f4
arnodmental <arnodmental@4cff5740-f916-11dd-b507-8b4ecd5210be>
2009-11-13 11:37:45 +0000

    Added basic file logging
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@145 4cff5740-f916-11dd-b507-8b4ecd5210be

---
+++ Log/file.php
@@ -0,0 +1,43 @@
+<?php
+class Log_file {
+  public function init($options)
+  {
+    $this->_options = $options;
+  }
+  public function dump($var)
+  {
+    $out='';
+    switch(true) {
+      case is_object($var):
+        foreach(get_object_vars($var) as $k=>$v) {
+          $out.="\t".$k.' = '.$v."\n";
+        }
+      break;  
+      case is_array($var):
+      foreach($var as $k=>$v) {
+        $out.="\t".$k.' = '.$v."\n";
+      }
+      break;
+      default:
+        $out.="\t".$k."\n";
+    }
+    $this->write($out,null,false);
+  }
+  public function section($title)
+  {
+    $this->write('########## '.$title.' ##########',null,false);
+  }
+  public function __call($meth,$args)
+  {
+    $this->write($args[0],$meth);
+  }
+  public function write($message,$level,$prepend = true)
+  {
+    if($prepend) {
+      $prefix = date('Y-m-d H:i:s').sprintf('%15s',strtoupper($level)).'::';
+    } else {
+      $prefix = '';
+    }
+    file_put_contents($this->_options['file'],$prefix.$message."\n",FILE_APPEND);
+  }
+}
