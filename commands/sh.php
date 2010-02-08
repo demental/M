fed9de0cfbad5447e881987b2139d1a05bec66e4
arnodmental <arnodmental@4cff5740-f916-11dd-b507-8b4ecd5210be>
2010-02-08 16:43:28 +0000

    Removed svn command. Added sh command.
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@211 4cff5740-f916-11dd-b507-8b4ecd5210be

---
+++ commands/sh.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * M PHP Framework
+ *
+ * @package      M
+ * @subpackage   sh.php
+ * @author       Arnaud Sellenet <demental@sat2way.com>
+ * @copyright    Copyright (c) 2003-2009 Arnaud Sellenet
+ * @license      http://opensource.org/licenses/lgpl-license.php GNU Lesser General Public License
+ * @version      0.1
+ */
+
+/**
+* command to launch shell commands
+*/
+class Command_Sh extends Command
+{
+  public function shortHelp()
+  {
+    $this->line('launches a shell command');
+  }
+  public function longHelp($params)
+  {
+    $this->line('Launches a shell command');
+    $this->line('Usage:');
+    $this->line('sh command_to_launch');
+  }
+  public function execute($params)
+  {
+    if(count($params) == 0) {
+      return $this->error('No shell command specified');
+    }
+    $params = implode(' ',$params);
+    $this->line('Launching shell command:'.$params);
+    passthru($params);
+  }
+}
