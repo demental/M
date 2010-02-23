13f20ff495243c3a1db87c2f811063403c54b56e
arnodmental <arnodmental@4cff5740-f916-11dd-b507-8b4ecd5210be>
2010-02-23 13:39:28 +0000

    Strings::stripify fix
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@226 4cff5740-f916-11dd-b507-8b4ecd5210be

--- Strings.php
+++ Strings.php
@@ -20,7 +20,7 @@ class Strings {
 		$string = trim($string);
 		if($varCompliant) {
 
-			$string=strtr(utf8_decode($string),utf8_decode(':+ëËÉÈÀÂÔÊÎÛàâéêèîôûùç² ,!?&\'"./'),'__eeeeaaoeiuaaeeeiouuc2_________');
+			$string=strtr(utf8_decode($string),utf8_decode('-:+ëËÉÈÀÂÔÊÎÛàâéêèîôûùç² ,!?&\'"./'),'___eeeeaaoeiuaaeeeiouuc2_________');
 			$sep='_';
 		} else {
 			$string=strtr(utf8_decode($string),utf8_decode('ëËÉÈÀÂÔÊÎÛàâéêèîôûùç² ,!?&\'"./'),'eeeeaaoeiuaaeeeiouuc2---------');
