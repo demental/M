0c851719f122083868a12709b23617b71ae0ebad
pascal.hubrecht <pascal.hubrecht@4cff5740-f916-11dd-b507-8b4ecd5210be>
2010-09-08 09:47:21 +0000

    * paypal driver update
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@325 4cff5740-f916-11dd-b507-8b4ecd5210be

--- Payment/Driver/PAYPAL.php
+++ Payment/Driver/PAYPAL.php
@@ -63,6 +63,6 @@ class Payment_Driver_PAYPAL extends Payment
     $this->transaction_id=$id;
   }
   public function getResponseCode() {
-    return ($this->transcript['PAYMENTSTATUS'] == 'Completed') ? '00' : $this->transcript['ACK'];
+    return ($this->transcript['PAYMENTSTATUS'] == 'Completed') ? '00' : $this->transcript['PAYMENTSTATUS'];
   }
 }
