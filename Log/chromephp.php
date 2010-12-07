03cc89da3a0c26eda7cd78fdfb8715d8b09aca54
pascal.hubrecht <pascal.hubrecht@4cff5740-f916-11dd-b507-8b4ecd5210be>
2010-12-07 09:09:40 +0000

    * New log driver : ChromePhp
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@345 4cff5740-f916-11dd-b507-8b4ecd5210be

---
+++ Log/chromephp.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'M/lib/chromephp/ChromePhp.php';
+
+ChromePhp::useFile(LOG_DRIVER_FILE, LOG_DRIVER_URL);
+
+
+class Log_chromephp {
+  public function message($message,$level)
+  {
+    switch ($level)
+    {
+      case 'error' :
+        ChromePhp::error($message);
+      break;
+      case 'warning' :
+        ChromePhp::warning($message);
+      break;
+      case 'info' :
+      default :
+        ChromePhp::log($message);
+      break;
+    }
+  }
+}
