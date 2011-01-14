685dafa33a73c968fa074b5da80eb91bd6b92a2b
arnodmental <arnodmental@4cff5740-f916-11dd-b507-8b4ecd5210be>
2011-01-14 13:39:43 +0000

    custom logging in DB
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@365 4cff5740-f916-11dd-b507-8b4ecd5210be

--- Plugins/log/DB/log.php
+++ Plugins/log/DB/log.php
@@ -25,19 +25,19 @@ class DB_DataObject_Plugin_Log extends M_Plugin
   
   public function postupdate($obj)
   {
-    $this->getLogger()->info(vsprintf('[%1$s %2$s U %3$s] %3$s has updated %1$s ID %2$s',array($obj->tableName(),$obj->pk(),self::getUsername())));
+    $this->getLogger()->info(vsprintf('[%1$s %2$s UPDATE %3$s] %3$s has updated %1$s ID %2$s',array($obj->tableName(),$obj->pk(),self::getUsername())));
   }
   public function postinsert($obj)
   {
-    $this->getLogger()->info(vsprintf('[%1$s %2$s U %3$s] %3$s has updated %1$s ID %2$s',array($obj->tableName(),$obj->pk(),self::getUsername())));
+    $this->getLogger()->info(vsprintf('[%1$s %2$s INSERT %3$s] %3$s has updated %1$s ID %2$s',array($obj->tableName(),$obj->pk(),self::getUsername())));
   }
   public function postdelete($obj)
   {
-    $this->getLogger()->info(vsprintf('[%1$s %2$s U %3$s] %3$s has updated %1$s ID %2$s',array($obj->tableName(),$obj->pk(),self::getUsername())));
+    $this->getLogger()->info(vsprintf('[%1$s %2$s DELETE %3$s] %3$s has updated %1$s ID %2$s',array($obj->tableName(),$obj->pk(),self::getUsername())));
   }
-  public function log($message, DB_DataObject $obj)
+  public function log($message, $messagecode, DB_DataObject $obj)
   {
-    self::getLogger()->info($message);
+    self::getLogger()->info(vsprintf('[%1$s %2$s %5$s %3$s] %3$s %4$s %1$s ID %2$s',array($obj->tableName(),$obj->pk(),self::getUsername(),$message,$messagecode)));
   }
   public static function getUsername()
   {
