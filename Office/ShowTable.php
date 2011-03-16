c7de04222197ecee4c405ba7f554aae0d8f35ff8
arnodmental <arnodmental@4cff5740-f916-11dd-b507-8b4ecd5210be>
2011-03-16 17:11:18 +0000

    possible office bugfix
    git-svn-id: https://m4php5.googlecode.com/svn/trunk@387 4cff5740-f916-11dd-b507-8b4ecd5210be

--- Office/ShowTable.php
+++ Office/ShowTable.php
@@ -79,7 +79,7 @@ class M_Office_ShowTable extends M_Office_Controller {
     }
 
           
-    if($this->getOption('view',$do->tableName())===TRUE || $this->getOption('view',$do->tableName())==1) {
+    if($this->getOption('view',$this->module)===TRUE) {
       require 'M/Office/View/DOPaging.php';
       $dg =  new M_Office_View_DOPaging($this);
       $this->assign('__listview','dopaging');
