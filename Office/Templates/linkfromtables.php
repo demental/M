6ac9356d8d21b83e8265708e9823c1ea7ed9e86b
arnaud sellenet <arnodmental@gmail.com>
2012-04-22 22:59:40 +0200

    linkfromtable addrecord links fix

--- Office/Templates/linkfromtables.php
+++ Office/Templates/linkfromtables.php
@@ -4,7 +4,7 @@
 <?php foreach($data as $arec):?>
   <li><a href="<?php echo $arec['link']?>"><?php echo $arec['nb']?> <?php echo $arec['tablename']?></a>
     <?php if($arec['add']):?>
-      <a href="<?php echo M_Office_Util::getQueryParams(array('module'=>$arec['table'],'addRecord'=>1,'filterField'=>$arec['linkField'],'filterValue'=>$do->{$arec['field']}),array('record'))?>">[+]</a>
+      <a href="<?php echo M_Office_Util::getQueryParams(array('module'=>$arec['table'],'addRecord'=>1,'filterField'=>$arec['linkField'],'filterValue'=>$do->{$arec['field']}),array('record','__record_ref'))?>">[+]</a>
     <?php endif?>  
     </li>
 <?php endforeach?>
