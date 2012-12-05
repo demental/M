8dca55d52c200d1a6fecfcbccd7f1f8ce3c1e97c
arnaud sellenet <arnodmental@gmail.com>
2012-12-05 10:45:00 +0100

    Wordpress: apply shortcodes optionnally (false by default)

--- WPTools.php
+++ WPTools.php
@@ -3,6 +3,9 @@ require_once 'Zend/XmlRpc/Client.php';
 /**
  * Wordpress integration into the M framework
  * Requires that yuo install the extendedapi plugin into wordpress.
+ * You also need to activate the following :
+ * * get_page_by_path
+ * * do_shortcode
  */
 class WPTools {
   protected static $_inited  = false;
@@ -13,7 +16,7 @@ class WPTools {
   public static $wp_root;
   public static $wp_login;
   public static $wp_password;
-  public function getPage($pageID)
+  public function getPage($pageID, $apply_shortcodes = false)
   {
     $options = array(
         'caching' =>true,
@@ -23,14 +26,16 @@ class WPTools {
 		);
 
 		$cache = new Cache_Lite($options);
-    $cacheName = 'wptool'.Config::get('wp_root').$pageID;
+    $cacheName = 'wptool'.Config::get('wp_root').$pageID.'_'.$apply_shortcodes;
 		if($_cachedData = $cache->get($cacheName)) {
       return unserialize($_cachedData);
     }
 
     $client = new Zend_XmlRpc_Client(self::$wp_root.'/xmlrpc.php');
     $c = (object)$client->getProxy(self::$namespace)->callWpMethod(self::$wp_login, self::$wp_password,'get_page_by_path', array($pageID));
-    $c->post_content = nl2br($c->post_content);
+    if($apply_shortcodes) {
+      $c->post_content = $client->getProxy(self::$namespace)->callWpMethod(self::$wp_login, self::$wp_password,'do_shortcode', array($c->post_content));
+    }
     $cache->save(serialize($c));
     return $c;
   }
