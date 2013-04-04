5cdf3d2ecb96d75aed1333c9ca9ebf14d92a8491
arnaud sellenet <arnodmental@gmail.com>
2013-04-04 11:09:06 +0200

    WIP generator command

---
+++ commands/generate.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * M PHP Framework
+ *
+ * @package      M
+ * @subpackage   generate.php
+ * @author       Arnaud Sellenet <demental@sat2way.com>
+ * @license      http://opensource.org/licenses/lgpl-license.php GNU Lesser General Public License
+ * @version      0.1
+ */
+
+/**
+ * Command to invoke generators
+ */
+
+class Command_generate extends Command {
+  public function shortHelp()
+  {
+    return $this->line('Invoke generators');
+  }
+
+  public function longHelp($params)
+  {
+    $this->shortHelp();
+    $this->line('Usage: ');
+    $this->line('generate migration [NAME_OF_MIGRATION] [FORMAT(php:default|sql)]');
+  }
+
+  public function execute($params)
+  {
+    $generator = array_shift($params);
+    if(!method_exists($this, 'generate_'.$generator)) return $this->error('No '.$generator.' generator');
+    call_user_func_array(array($this,'generate_'.$generator), $params);
+  }
+
+  public function generate_migration($name, $format = 'php')
+  {
+
+  }
+}
