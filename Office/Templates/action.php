ca2e015edd0b5975b4942ae29f7986afb6d3f88c
arnaud sellenet <arnodmental@gmail.com>
2013-06-19 15:58:56 +0200

    use Mtpl::addJS for downloadable actions

--- Office/Templates/action.php
+++ Office/Templates/action.php
@@ -1,12 +1,14 @@
 <?php $this->i('form-discrete',array('form'=>$actionform,'cancel'=>true,'cancelurl'=>M_Office_Util::getQueryParams(array(),array('doSingleAction','glaction','doaction'))))?>
 <?php if($isdownload):?>
-<script type="text/javascript">
-var nextDone = false;
+<?php $this->startCapture('js')?>
+
+  var nextDone = false;
 $('form').bind('submit',function(){
   if(!nextDone) {
     $('<input type="submit" value=">> <?php _e('Next')?>" name="__submitnext__"/>').appendTo($(this).find('.submitbutton'));
   }
   nextDone = true;
 });
-</script>
+
+<?php $this->endCapture(); Mtpl::addJSinline($this->getCapture('js'))?>
 <?php endif?>
