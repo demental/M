3443b2f9c913549b5fda68ac65aff09a6f892d97
arnaud sellenet <arnodmental@gmail.com>
2013-09-05 14:30:25 +0200

    nullify body if not html

--- Mail/Mandrill.php
+++ Mail/Mandrill.php
@@ -62,6 +62,10 @@ class Mail_mandrill extends Maman implements iMailDriver {
         $attachments_request[]= $arr;
       }
     }
+    if(!$html) {
+      $altbody = $body;
+      $body = null;
+    }
 
     $message = array(
       'html' => $body,
