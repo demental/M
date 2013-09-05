d53aad127f9bb0d521c1ba913dd425a56e2b0d15
arnaud sellenet <arnodmental@gmail.com>
2013-09-05 13:22:15 +0200

    spaces

--- Mail/Phpmailer.php
+++ Mail/Phpmailer.php
@@ -19,52 +19,52 @@ class Mail_phpmailer extends Maman implements iMailDriver {
 			$from=array($from,$from);
 		}
 
-			$mail = new phpmailer();
-			$mail->PluginDir='M/lib/phpmailer/';
+		$mail = new phpmailer();
+		$mail->PluginDir='M/lib/phpmailer/';
 
-			if($this->getConfig('smtp')) {
+		if($this->getConfig('smtp')) {
+			$mail->isSMTP();
+			$mail->Host=$this->getConfig('smtphost');
+      if($this->getConfig('smtpusername')) {
+        $mail->SMTPAuth = true;
+        $mail->Port = $this->getConfig('smtpport') ? $this->getConfig('smtpport') : 25;
+        $mail->SMTPDebug = $this->smtpdebug;
+        $mail->Username = $this->getConfig('smtpusername');
+        $mail->Password = $this->getConfig('smtppassword');
+      }
+		}
 
-				$mail->isSMTP();
-				$mail->Host=$this->getConfig('smtphost');
-        if($this->getConfig('smtpusername')) {
-          $mail->SMTPAuth = true;
-          $mail->Port = $this->getConfig('smtpport') ? $this->getConfig('smtpport') : 25;
-          $mail->SMTPDebug = $this->smtpdebug;
-          $mail->Username = $this->getConfig('smtpusername');
-          $mail->Password = $this->getConfig('smtppassword');
-        }
-			}
-			$mail->CharSet=$this->getConfig('encoding');
-			$mail->AddAddress($to);
-			$mail->Subject = $subject;
-			$mail->Body = $note.$body;
-			$mail->AltBody=$altbody;
-			if(!is_array($from)){
-				$from=array($from,$from);
-			}
-			$mail->From = $from[0];
-			$mail->FromName = $from[1];
+		$mail->CharSet=$this->getConfig('encoding');
+		$mail->AddAddress($to);
+		$mail->Subject = $subject;
+		$mail->Body = $note.$body;
+		$mail->AltBody=$altbody;
+		if(!is_array($from)){
+			$from=array($from,$from);
+		}
+		$mail->From = $from[0];
+		$mail->FromName = $from[1];
 
-			if(key_exists('reply-to',$options)){
-				$mail->AddReplyTo($options['reply-to']);
-				unset($options['reply-to']);
-			}
-			if(key_exists('Sender',$options)) {
-				$mail->Sender = $options['Sender'];
-			}
+		if(key_exists('reply-to',$options)){
+			$mail->AddReplyTo($options['reply-to']);
+			unset($options['reply-to']);
+		}
+		if(key_exists('Sender',$options)) {
+			$mail->Sender = $options['Sender'];
+		}
 
-			if(null!=$attachments){
-				if(!is_array($attachments)){
-					$attachments = array($attachments);
+		if(null!=$attachments){
+			if(!is_array($attachments)){
+				$attachments = array($attachments);
+			}
+			foreach($attachments as $k=>$v){
+				if(!$mail->AddAttachment($v, basename($v))){
+					trigger_error("Attachment $v could not be added");
 				}
-				foreach($attachments as $k=>$v){
-					if(!$mail->AddAttachment($v, basename($v))){
-						trigger_error("Attachment $v could not be added");
-					}
-				}
 			}
-			$mail->IsHTML($html);
-			$result = 	$mail->send();
-    }
+		}
+		$mail->IsHTML($html);
+		$result = 	$mail->send();
+  }
 
 }
