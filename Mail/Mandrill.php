72ccc5f20b375d4ff54b91bfc35c710590cf2f55
arnaud sellenet <arnodmental@gmail.com>
2013-09-05 13:30:48 +0200

    pre php 5.3 syntax

--- Mail/Mandrill.php
+++ Mail/Mandrill.php
@@ -80,12 +80,8 @@ class Mail_mandrill extends Maman implements iMailDriver {
       'message' => $message
     );
 
-    $request_json = json_encode($request);
-
-      '{"type":"messages","call":"send","message":{"html": "<h1>example html</h1>", "text": "example text", "subject": "example subject", "from_email": "wes@werxltd.com", "from_name": "example from_name", "to":[{"email": "wes@reasontostand.org", "name": "Wes Widner"}],"headers":{"...": "..."},"track_opens":true,"track_clicks":true,"auto_text":true,"url_strip_qs":true,"tags":["test","example","sample"],"google_analytics_domains":["werxltd.com"],"google_analytics_campaign":["..."],"metadata":["..."]}}';
-
 //    $ret = Mandrill::call($request);
-    $ret = Mandrill::__callStatic('call', $request);
+    $ret = Mandrill::__callStatic('call', array($request));
 
   }
 
