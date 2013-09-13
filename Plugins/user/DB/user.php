5432bfbc9f8ba8d1970cd28722074ed7d6401cd6
arnaud sellenet <arnodmental@gmail.com>
2013-09-13 11:38:35 +0200

    sign in translations

--- Plugins/user/DB/user.php
+++ Plugins/user/DB/user.php
@@ -51,11 +51,11 @@ class DB_DataObject_Plugin_User extends M_Plugin
               $obj->fb_fieldLabels[$defs['pwd']]);
 
             if(empty($obj->id)) {
-              $form->addRule($fb->elementNamePrefix.$defs['pwd'].$fb->elementNamePostfix,__('Veuillez entrer un mot de passe'),'required');
-              $form->addRule($fb->elementNamePrefix.$defs['pwd'].'2'.$fb->elementNamePostfix,__('Veuillez retaper votre mot de passe'),'required');
+              $form->addRule($fb->elementNamePrefix.$defs['pwd'].$fb->elementNamePostfix,__('user.register.error.required_password'),'required');
+              $form->addRule($fb->elementNamePrefix.$defs['pwd'].'2'.$fb->elementNamePostfix,__('user.register.error.required_password'),'required');
             }
             $form->insertElementBefore($pwd2,$fb->elementNamePrefix.$defs['pwd'].$fb->elementNamePostfix);
-            $form->addRule(array($fb->elementNamePrefix.$defs['pwd'].'2'.$fb->elementNamePostfix,$fb->elementNamePrefix.$defs['pwd'].$fb->elementNamePostfix),__('Vos deux mots de passe ne correspondent pas'),'compare');
+            $form->addRule(array($fb->elementNamePrefix.$defs['pwd'].'2'.$fb->elementNamePostfix,$fb->elementNamePrefix.$defs['pwd'].$fb->elementNamePostfix),__('user.register.error.unmatched_password'),'compare');
             $form->addFormRule(array($this,'checkUniqueLogin'));
 
         }
@@ -69,7 +69,7 @@ class DB_DataObject_Plugin_User extends M_Plugin
         $test->whereAdd("id!='".$this->_obj->id."'");
         $test->{$defs['login']} = $values[$defs['login']];
         if($test->find(true)) {
-          return array($defs['login']=>__('This username already exists, please choose another one'));
+          return array($defs['login']=>__('user.register.error.already_exists'));
         }
         return true;
 	}
@@ -101,11 +101,11 @@ class DB_DataObject_Plugin_User extends M_Plugin
       $this->_addreminder = $reminder;
 
       if($register) {
-          $obj->fb_formHeaderText = __('Déjà inscrit ?');
+          $obj->fb_formHeaderText = __('user.login.title.already_signedin');
       } else {
-          $obj->fb_formHeaderText = __('Identification');
+          $obj->fb_formHeaderText = __('user.login.title.main');
       }
-      $obj->fb_submitText = '>> '.__('Valider');
+      $obj->fb_submitText = '>> '.__('user.login.link.submit');
       $this->_usercontext = $defs['context'];
       $obj->fb_fieldsToRender = array($defs['login'],$defs['pwd']);
       $obj->fb_preDefOrder = array($defs['login'],$defs['pwd']);
@@ -119,20 +119,20 @@ class DB_DataObject_Plugin_User extends M_Plugin
   {
     $defs = $obj->_getPluginsDef();
     $defs = $defs['user'];
-      $obj->fb_formHeaderText = __('Mot de passe perdu ?');
-      $obj->fb_submitText = '>> '.__('Envoyer');
+      $obj->fb_formHeaderText = __('user.reminder.title');
+      $obj->fb_submitText = '>> '.__('user.reminder.submit');
       $obj->fb_fieldsToRender = array($defs['email']);
       $obj->fb_postGenerateFormCallback=array($this,'postGenerateReminder');
   }
   function postGenerateLogin(&$form,&$fb,$obj = null) {
       if($this->_addreminder) {
-          $elt = HTML_QuickForm::createElement('static','remindertext','','<a href="'.$this->reminderUrl.'">'.__('Mot de passe perdu ?').'</a>');
+          $elt = HTML_QuickForm::createElement('static','remindertext','','<a href="'.$this->reminderUrl.'">'.__('user.login.link.reminder').'</a>');
           $form->insertElementBefore($elt,'__submit__');
       }
       if($this->_addregister) {
-          $form->addElement('header','newUser',__('Pas encore inscrit ?'));
+          $form->addElement('header','newUser',__('user.login.register.title'));
           if($this->registerUrl) {
-              $form->addElement('static','newUserLink','','<a href="'.$this->registerUrl.'">'.__('Inscrivez-vous').'</a>');
+              $form->addElement('static','newUserLink','','<a href="'.$this->registerUrl.'">'.__('user.login.link.register').'</a>');
           }
       }
       $this->_obj = $fb->_do;
@@ -142,9 +142,9 @@ class DB_DataObject_Plugin_User extends M_Plugin
 
     $defs = $this->_obj->_getPluginsDef();
     $defs = $defs['user'];
-    $noaccountError = $defs['noaccountError']?$defs['noaccountError']:__('Ce compte n\'existe pas');
-    $passwordError = $defs['passwordError']?$defs['passwordError']:__('Mot de passe incorrect');
-    $inactiveError = $defs['inactiveError']?$defs['inactiveError']:__('Ce compte n\'est pas actif');
+    $noaccountError = $defs['noaccountError']?$defs['noaccountError']:__('user.login.error.noaccout');
+    $passwordError = $defs['passwordError']?$defs['passwordError']:__('user.login.error.wrongpassword');
+    $inactiveError = $defs['inactiveError']?$defs['inactiveError']:__('user.login.error.inactiveaccount');
 
       if(User::getInstance($this->_usercontext)->login($values[$defs['login']],$values[$defs['pwd']])) {
           if(!empty($defs['valid']) && !User::getInstance($this->_usercontext)->getDBDO()->{$defs['valid']}) {
@@ -180,7 +180,7 @@ class DB_DataObject_Plugin_User extends M_Plugin
       $do = $fb->_do;
       $defs = $do->_getPluginsDef();
       $defs = $defs['user'];
-      $elt = HTML_QuickForm::createElement('static','remindertext','',__('To retreive your password, enter your email address in the field below, then you will be sent your new password.'));
+      $elt = HTML_QuickForm::createElement('static','remindertext','',__('user.reminder.text'));
       $form->insertElementBefore($elt,$fb->elementNamePrefix.$defs['email'].$fb->elementNamePostfix);
   }
   /**
