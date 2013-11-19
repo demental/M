46be3d9ec187884499f2c8d9bb2f5bd05c32a6f6
arnaud sellenet <arnodmental@gmail.com>
2013-11-19 11:04:20 +0100

    cherry-pick deny_unless_can

--- Office/functions.php
+++ Office/functions.php
@@ -25,3 +25,11 @@ function can($action, $module, $record = null) {
   }
   return $result ? true : false;
 }
+
+function deny_unless_can($action, $module, $record = null)
+{
+  if(can($action, $module, $record)) return;
+
+  throw new SecurityException(__('error.cannot_do_action_on_that', array($action, ($record ? $record : __("modules.$module.frontname")))));
+
+}
