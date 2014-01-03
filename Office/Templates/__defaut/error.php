2c1978cbad71f35dc065a48753dde6257a627181
arnaud sellenet <arnodmental@gmail.com>
2014-01-03 15:29:15 +0100

    error page

--- Office/Templates/__defaut/error.php
+++ Office/Templates/__defaut/error.php
@@ -1,1 +1,32 @@
-ERREUR
+<?php
+Mtpl::addJS('livesearch');
+Mtpl::addCSS('livesearch');
+Mtpl::addJSinline('$("#chooseTable input").livesearch({autosearch:true,url:"'.ROOT_ADMIN_URL.ROOT_ADMIN_SCRIPT.'?livesearch=1"'.',minchar:2});','ready');
+?>
+<!DOCTYPE html>
+<html lang="<?php echo T::getLocale()?>">
+	<head>
+	  <meta charset="utf-8" />
+	  <title><?php echo $adminTitle ?></title>
+  	<link rel="stylesheet" type="text/css" media="screen,print" href="/css/reset-fonts-grids.css" />
+
+  	<link rel="stylesheet" type="text/css" media="screen,print" href="/css/styleforms.css" />
+  	<link rel="stylesheet" type="text/css" media="screen,print" href="/css/style_admin.css" />
+  </head>
+  <body>
+		<div id="doc3">
+
+    <div id="bd">
+      <div class="global_error">
+        <h1><?php _e('title.error')?></h1>
+        <h2><i class="icon icon-warning-sign"></i> <?php echo $message ?></h2>
+        <?php if('development' == MODE):?>
+          <pre><?php echo $error->getTraceAsString()?></pre>
+        <?php endif?>
+        <p><a class="btn btn-large" href="javascript:history.back()"><?php _e('button.back_one_page')?></a>
+          <a class="btn btn-warning btn-large" href="<?php echo ROOT_ADMIN_URL?>"><?php _e('button.back_home')?></a></p>
+      </div>
+</div>
+</div>
+</body>
+</html>
