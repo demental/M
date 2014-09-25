d93d575f4927e1314f983d3f9b9d97dd04f9178d
arnaud sellenet <arnodmental@gmail.com>
2014-09-25 23:05:09 +0200

    typo

--- Office/treesort.php
+++ Office/treesort.php
@@ -9,21 +9,19 @@
 *
 * This controller is called by the tree view when drag-and-drop sorting
 * done by the user
-* 
-* 
+*
+*
 *
 * @package      M
 * @subpackage   M_Office
-* @author       Arnaud Sellenet <demental@sat2way.com>
-
+* @author       Arnaud Sellenet
 * @license      http://opensource.org/licenses/lgpl-license.php GNU Lesser General Public License
 * @version      0.1
 * @todo         This solution is not really clean, should be refactored.
 */
-*/
 
 class M_Office_treesort extends M_Office_Controller
-{    
+{
     function M_Office_treesort() {
         M_Office_Controller::M_Office_Controller();
     }
