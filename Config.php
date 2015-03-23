6cce5250c5fc6265c3d0fd4c137872d81b2c52fe
arnaud sellenet <arnodmental@gmail.com>
2015-03-23 12:39:33 +0100

    reduce database queries loading all preferences at once

--- Config.php
+++ Config.php
@@ -17,44 +17,44 @@
 class Config
 {
 
-	/**
-	 * associative array that stores application preferences
-	 * Application preferences are configurable in the application's database
-	 * This array is used as a basic caching system by Config::getPref()
-	 *
-	 * @var $preftab array
-	 * @access protected
-	 */
-	protected static $prefArr = array();
+  /**
+   * associative array that stores application preferences
+   * Application preferences are configurable in the application's database
+   * This array is used as a basic caching system by Config::getPref()
+   *
+   * @var $preftab array
+   * @access protected
+   */
+  protected static $prefArr = array();
 
 
-	/**
-	 *
-	 * associative array that stores application configuration values
-	 * all the configuration values are stored in this array at runtime,
-	 * using Config::load($arr) in M_Setup::setUpEnv()
-	 *
-	 * @var		array
-	 * @access	protected
-	 * @
-	 */
-	protected static $cfgArr = array();
+  /**
+   *
+   * associative array that stores application configuration values
+   * all the configuration values are stored in this array at runtime,
+   * using Config::load($arr) in M_Setup::setUpEnv()
+   *
+   * @var   array
+   * @access  protected
+   * @
+   */
+  protected static $cfgArr = array();
 
   protected static $prefFile;
-	/**
-	 *
-	 * Loads a configuration array
-	 * if called several times, consecutive arrays are merged.
-	 *
-	 * @access	public
-	 * @param	array	$array Associative array containing app's configuration values
-	 * @static
-	 *
-	 */
-	public static function load($array)
-	{
-		self::$cfgArr = array_merge(self::$cfgArr,$array);
-	}
+  /**
+   *
+   * Loads a configuration array
+   * if called several times, consecutive arrays are merged.
+   *
+   * @access  public
+   * @param array $array Associative array containing app's configuration values
+   * @static
+   *
+   */
+  public static function load($array)
+  {
+    self::$cfgArr = array_merge(self::$cfgArr,$array);
+  }
 
   /**
    * Returns (an creates if not set yet) the preference file name
@@ -67,17 +67,17 @@ class Config
      }
      return self::$prefFile;
    }
-	/**
-	 *
-	 * Loads a preference file into an array
-	 *
-	 * @access	public
-	 * @param	string	$file filename with path
-	 * @static
-	 *
-	 */
-	public static function loadPrefFile()
-	{
+  /**
+   *
+   * Loads a preference file into an array
+   *
+   * @access  public
+   * @param string  $file filename with path
+   * @static
+   *
+   */
+  public static function loadPrefFile()
+  {
     $file = APP_ROOT . 'app/cache/' . self::getPrefFile();
     if (!file_exists($file))
     {
@@ -85,18 +85,36 @@ class Config
       self::savePrefFile();
     }
     self::$prefArr = unserialize(file_get_contents($file));
-	}
-	/**
-	 *
-	 * Generate a preference file from database configuration table
-	 *
-	 * @access	public
-	 * @param	string	$file filename with path
-	 * @static
-	 *
-	 */
-	public static function savePrefFile()
-	{
+  }
+
+  protected static function loadPrefsFromDatabase()
+  {
+    $p = DB_DataObject::factory('preferences');
+    $p->find();
+    while($p->fetch()) {
+      self::$prefArr[$p->var] = self::parsePref($p);
+    }
+  }
+
+  protected static function loadPrefs($use_cache)
+  {
+    if($use_cache) {
+      self::loadPrefFile();
+    } else {
+      self::loadPrefsFromDatabase();
+    }
+  }
+  /**
+   *
+   * Generate a preference file from database configuration table
+   *
+   * @access  public
+   * @param string  $file filename with path
+   * @static
+   *
+   */
+  public static function savePrefFile()
+  {
     $file = APP_ROOT . 'app/cache/' . self::getPrefFile();
     $setup = Mreg::get('setup');
     if(is_object($setup)) {
@@ -109,7 +127,7 @@ class Config
       self::$prefArr[$prefs->var] = self::parsePref($prefs);
     }
     file_put_contents($file, serialize(self::$prefArr));
-	}
+  }
 
   public static function parsePref($pref)
   {
@@ -133,116 +151,113 @@ class Config
     }
   }
 
-	/**
-	 *
-	 * Get Alternate Languages (i.e. all languages that are handled by the application except the default language)
-	 *
-	 * @access	public
-	 * @static
-	 *
-	 */
-	public static function getAlternateLangs()
-	{
-		$l=Config::getAllLangs();
-		$cur=array_search(self::get('defaultLang'),$l);
-		unset($l[$cur]);
-		return $l;
-	}
+  /**
+   *
+   * Get Alternate Languages (i.e. all languages that are handled by the application except the default language)
+   *
+   * @access  public
+   * @static
+   *
+   */
+  public static function getAlternateLangs()
+  {
+    $l=Config::getAllLangs();
+    $cur=array_search(self::get('defaultLang'),$l);
+    unset($l[$cur]);
+    return $l;
+  }
 
-	/**
-	 *
-	 * Get all language/country pairs
-	 *
-	 * @access	public
-	 * @return	array indexed array of all the languages/country iso2iso2 codes handled by the application
-	 * @static
-	 *
-	 */
-	public static function getAllLangs()
-	{
-		return Config::get('installedLangs');
-	}
+  /**
+   *
+   * Get all language/country pairs
+   *
+   * @access  public
+   * @return  array indexed array of all the languages/country iso2iso2 codes handled by the application
+   * @static
+   *
+   */
+  public static function getAllLangs()
+  {
+    return Config::get('installedLangs');
+  }
 
-	/**
-	 *
-	 * Get all languages
-	 *
-	 * @access	public
-	 * @return	array indexed array of all the languages iso2 codes handled by the application
-	 * @static
-	 *
-	 */
-	public static function getAllLocales()
-	{
-		$ret = Config::get('installedLocales');
-		if(!is_array($ret)) {
+  /**
+   *
+   * Get all languages
+   *
+   * @access  public
+   * @return  array indexed array of all the languages iso2 codes handled by the application
+   * @static
+   *
+   */
+  public static function getAllLocales()
+  {
+    $ret = Config::get('installedLocales');
+    if(!is_array($ret)) {
       $ret = array();
-		  foreach(self::getAllLangs() as $pair) {
-		    $ret[] = substr($pair,0,2);
-		  }
-		  self::set('installedLocales',array_unique($ret));
-		}
-		return $ret;
-	}
-
-	/**
-	 *
-	 * Get config value
-	 *
-	 * @access	public
-	 * @param	string	$var	Variable to get
-	 * @return	string	Value
-	 * @static
-	 *
-	 */
-	public static function get($var)
-	{
-		return self::$cfgArr[$var];
-	}
-
-	/**
-	 *
-	 * Set config value
-	 *
-	 * @access	public
-	 * @param	string	$var	Variable to set
-	 * @param	string	$val	Value
-	 * @static
-	 *
-	 */
-	public static function set($var,$val)
-	{
-		self::$cfgArr[$var]=$val;
-	}
-
-	/**
-	 *
-	 * Get preferences value.
-	 * Preferences are values stored in the application's database,
-	 * in a table called 'preferences'
-	 * allowing administrators to modify them through a web interface
-	 *
-	 * @access	public
-	 * @param	string	$var	Variable to get
-   * @param bool default true use cache file is true, database otherwise
-	 * @return	string	Value
-	 * @static
-	 *
-	 */
-
-
-	public static function getPref($var, $use_cache = null)
-	{
-		if(is_null($use_cache)) { $use_cache = MODE == 'production'; }
-    if(!$use_cache) {
-      return self::getPrefFromDatabase($var);
+      foreach(self::getAllLangs() as $pair) {
+        $ret[] = substr($pair,0,2);
+      }
+      self::set('installedLocales',array_unique($ret));
     }
-		if(!key_exists($var,self::$prefArr))
-		{
-		  self::loadPrefFile();
-		}
-		return self::$prefArr[$var];
-	}
+    return $ret;
+  }
+
+  /**
+   *
+   * Get config value
+   *
+   * @access  public
+   * @param string  $var  Variable to get
+   * @return  string  Value
+   * @static
+   *
+   */
+  public static function get($var)
+  {
+    return self::$cfgArr[$var];
+  }
+
+  /**
+   *
+   * Set config value
+   *
+   * @access  public
+   * @param string  $var  Variable to set
+   * @param string  $val  Value
+   * @static
+   *
+   */
+  public static function set($var,$val)
+  {
+    self::$cfgArr[$var]=$val;
+  }
+
+  /**
+   *
+   * Get preferences value.
+   * Preferences are values stored in the application's database,
+   * in a table called 'preferences'
+   * allowing administrators to modify them through a web interface
+   *
+   * @access  public
+   * @param string  $var  Variable to get
+   * @param bool default true use cache file is true, database otherwise
+   * @return  string  Value
+   * @static
+   *
+   */
+
+
+  public static function getPref($var, $use_cache = null)
+  {
+    if(is_null($use_cache)) { $use_cache = MODE == 'production'; }
+    if(!key_exists($var,self::$prefArr))
+    {
+      self::loadPrefs($use_cache);
+    }
+    return self::$prefArr[$var];
+  }
 
   /**
    * Get preferences value from database storage
@@ -258,29 +273,29 @@ class Config
     return self::parsePref($pref);
   }
 
-	/**
-	 *
-	 * Set preferences values - affects database
-	 *
-	 * @access	public
-	 * @param	string	$var	Variable to set
-	 * @param	string	$val	Value
-	 * @return	string Value
-	 * Note : if it does not exist in database, the pref is created.
-	 * @static
-	 *
-	 */
-	public static function setPref($var,$val)
-	{
-		$res = DB_DataObject::factory('preferences');
-		$res->var=$var;
-		if(!$res->find(true)) {
-			$res->insert();
-		}
-		$res->val = $val;
-		$res->update();
-		self::$prefArr[$var]=$val;
-		if(MODE == 'production') self::savePrefFile();
-		return self::$prefArr[$var];
-	}
+  /**
+   *
+   * Set preferences values - affects database
+   *
+   * @access  public
+   * @param string  $var  Variable to set
+   * @param string  $val  Value
+   * @return  string Value
+   * Note : if it does not exist in database, the pref is created.
+   * @static
+   *
+   */
+  public static function setPref($var,$val)
+  {
+    $res = DB_DataObject::factory('preferences');
+    $res->var=$var;
+    if(!$res->find(true)) {
+      $res->insert();
+    }
+    $res->val = $val;
+    $res->update();
+    self::$prefArr[$var]=$val;
+    if(MODE == 'production') self::savePrefFile();
+    return self::$prefArr[$var];
+  }
 }
