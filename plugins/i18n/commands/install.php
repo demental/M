b4b836e8f6d17d7e678b270181f8990011340c90
arnaud sellenet <arnodmental@gmail.com>
2015-03-24 11:37:31 +0100

    move most form related logic from i18n plugin to its formbuilder

--- plugins/i18n/commands/install.php
+++ plugins/i18n/commands/install.php
@@ -30,9 +30,194 @@ class i18n_Command_Install extends Command {
     $tdef = $t->_getPluginsDef();
     if($tdef['i18n']) {
       echo $table.' has i18n fields, generating '.$table.'_i18n table'."\n";
-      $t->getPlugin('i18n')->generateTable($t);
+      $this->generateTable($t);
     } else {
       $this->line($table.' : no i18n');
     }
   }
+
+  // =========================================
+  // = i18n table Generation (not migration) =
+  // =========================================
+  protected function generateTable($obj)
+  {
+    $iname = $obj->tableName().'_i18n';
+    $res = $this->migration_createI18nTable($obj,$iname);
+    if(PEAR::isError($res)) {
+      trigger_error('failed creating '.$iname.' : '.$res->getMessage().' : '.$res->userinfo,E_USER_WARNING);
+      $obj->rollback();
+      return false;
+    }
+    $res = $this->migration_createI18nIndexes($obj,$iname);
+    if(PEAR::isError($res)) {
+      trigger_error('failed creating indexes for '.$iname.' : '.$res->getMessage().' : '.$res->userinfo,E_USER_WARNING);
+      $obj->rollback();
+      return false;
+    }
+    $res = $this->migration_copyDataToI18n($obj,$iname);
+    $res = $this->migration_removeNonI18nFields($obj,$iname);
+    if(PEAR::isError($res)) {
+      trigger_error('failed removing non i18n fields for '.$iname.' : '.$res->getMessage().' : '.$res->userinfo,E_USER_WARNING);
+      $obj->rollback();
+      return false;
+    }
+    $res = $this->migration_rebuildObjects($obj,$iname);
+    $res = $this->migration_removeI18nFieldsFromOriginal($obj,$iname);
+    if(PEAR::isError($res)) {
+      trigger_error('failed removing i18n fields from '.$obj->tableName().' : '.$res->getMessage().' : '.$res->userinfo,E_USER_WARNING);
+      $obj->rollback();
+      return false;
+    }
+  }
+  public function migration_createI18nTable($obj,$iname)
+  {
+    echo 'duplicating table';
+    $db = $obj->getDatabaseConnection();
+    $res = $db->query('create table '.$iname.' LIKE '.$obj->tableName());
+    if(PEAR::isError($res)) {
+      trigger_error($res->getMessage(),E_USER_WARNING);
+      return $res;
+    }
+    echo 'finish duplicating table';
+    return true;
+  }
+  public function migration_createI18nIndexes($obj,$iname)
+  {
+    echo 'creating indexes';
+    $db = $obj->getDatabaseConnection();
+    $res = $db->loadModule('manager',null,true);
+    if(PEAR::isError($res)) {
+      throw new Exception($res->getMessage());
+    }
+
+    $res = $db->manager->alterTable($iname,array(
+      'remove'=>array('id'=>array()),'add'=>array('i18n_id'=>array('type'=>'integer','notnull'=>1,'default'=>0,'unsigned'=>1,'autoincrement'=>1,'primary'=>1))),false);
+    if(PEAR::isError($res)) {
+      return $res;
+    }
+    // Changing foreign key format if officepack used (CHAR(36))
+    if($obj->hasplugin('officepack') || $obj->hasplugin('guid')) {
+      $foreignkeyspecs = array('type'=>'text','length'=>36);
+    } else {
+      $foreignkeyspecs = array('type'=>'integer','unsigned'=>1);
+    }
+
+    $res2 = $db->manager->alterTable($iname,array(
+      'add'=>array( 'i18n_lang'=>array('type'=>'text','length'=>2,'notnull'=>1,'default'=>'fr'),
+                    'i18n_record_id'=>array_merge($foreignkeyspecs,array('notnull'=>1,'default'=>0)),
+                  )
+                ),false
+              );
+    if(PEAR::isError($res2)) {
+      return $res2;
+    }
+
+    $res3 = $db->createIndex($iname,'i18n',array('fields'=>array('i18n_lang'=>array(),'i18n_record_id'=>array())));
+    if(PEAR::isError($res3)) {
+      return $res3;
+    }
+    echo 'end creating indexes';
+    return true;
+  }
+  public function migration_getNonI18nFields($obj,$iname)
+  {
+    $info = $obj->_getPluginsDef();
+    $info = $info['i18n'];
+    $t = $obj->table();
+    $toremove = array();
+    $i18n = $info;
+    $keys = $obj->keys();
+    foreach($t as $field=>$info) {
+      if(!in_array($field,$i18n) && $field!=$keys[0]) {
+        $toremove[$field] = array();
+      }
+    }
+    return $toremove;
+  }
+  public function migration_removeNonI18nFields($obj,$iname)
+  {
+    $db = $obj->getDatabaseConnection();
+    $db->loadModule('manager',null,true);
+    $res = $db->manager->alterTable($iname,array('remove'=>$this->migration_getNonI18nFields($obj,$iname)),false);
+    if(PEAR::isError($res)) {
+      return $res;
+    }
+    return true;
+  }
+  public function migration_copyDataToI18n($obj,$iname)
+  {
+    echo 'copying data';
+    $info = $obj->_getPluginsDef();
+    $info = $info['i18n'];
+    $db = $obj->getDatabaseConnection();
+    foreach(Config::getAllLangs() as $lang) {
+      T::setLang($lang);
+      $original = DB_DataObject::factory($obj->tableName());
+      $original->_loadPlugins();
+      echo 'unloading plugins';
+      $original->unloadPlugin('i18n');
+      $ifields = $info;
+      unset($original->i18nFields);
+
+      $original->find();
+
+      $fieldsToInsert = array_merge(array('i18n_lang','i18n_record_id'),$ifields);
+      foreach($fieldsToInsert as $k=>$field) {
+        $fieldsToInsert[$k] = $db->quoteIdentifier($field);
+      }
+      while($original->fetch()) {
+        echo 'fetching record num '.$original->pk()."\n";
+        $valuesToInsert = array();
+        foreach($ifields as $field) {
+          if(is_numeric($original->{$field})) {
+            $valuesToInsert[]=$original->{$field};// This might never happen... we never know
+          } else {
+            $valuesToInsert[]=$db->quote($original->{$field});
+          }
+        }
+        $valuesToInsert = array_merge(array($db->quote($lang),$db->quote($original->pk())),$valuesToInsert);
+        $res = $db->query('INSERT INTO '.$db->quoteIdentifier($iname).' ('.implode(',',$fieldsToInsert).') VALUES('.implode(',',$valuesToInsert).')');
+        if(PEAR::isError($res)) {
+          $nbfailed[$lang]++;
+        }
+      }
+    }
+    if(is_array($nbfailed)) {
+      echo 'Failures while trying to insert translated data :<br />';
+      foreach($nbfailed as $lang=>$nb) {
+        echo $lang.' : '.$nb.'<br />';
+      }
+      echo '<br /><br />';
+    }
+    return true;
+  }
+  public function migration_removeI18nFieldsFromOriginal($obj,$iname)
+  {
+    $db = $obj->getDatabaseConnection();
+    $res = $db->loadModule('manager',null,true);
+    if(PEAR::isError($res)) {
+      throw new Exception($res->getMessage());
+    }
+    $info = $obj->_getPluginsDef();
+    $info = $info['i18n'];
+
+    $toremove = array_flip($info);
+    foreach($toremove as $k=>$v) {
+      $toremove[$k] = array();
+    }
+    $res = $db->manager->alterTable($obj->tableName(),array('remove'=>$toremove),false);
+    if(PEAR::isError($res)) {
+      return $res;
+    }
+    return true;
+  }
+  public function migration_rebuildObjects($obj,$iname)
+  {
+    $options = &PEAR::getStaticProperty('DB_DataObject', 'options');
+    $options['generator_include_regex']= '`^('.$obj->tableName().'|'.$iname.')$`';
+    $generator = new DB_DataObject_Advgenerator();
+    $generator->start();
+    return true;
+  }
+
 }
--- plugins/i18n/formbuilder.php
+++ plugins/i18n/formbuilder.php
@@ -23,7 +23,7 @@ class PluginI18nFormbuilder {
     }
 
     if($this->_grouped) {
-      $this->postGenerateFormGrouped($form,$fb,$info,$langs,$obj);
+      $this->after_form_grouped($form,$fb,$info,$langs);
       return;
     }
 
@@ -54,6 +54,189 @@ class PluginI18nFormbuilder {
         $elem->setAttribute('class',$elem->getAttribute('class').($elem->getAttribute('class')?' ':'').$class);
       }
     }
+  }
+
+  public function after_save(&$values,$fb)
+  {
+    $obj = $fb->_do;
+    foreach($this->getLocales($obj) as $lang) {
+      $obj->_i18ndos[$lang]->i18n_record_id = $obj->pk();
+      $obj->_i18nfbs[$lang]->processForm($values);
+    }
+  }
+
+
+  public function after_form_grouped($form,$fb,$info,$langs)
+  {
+    $obj = $fb->_do;
+    $lang = $langs[0];
+      $elements = $obj->_i18nfbs[$lang]->_reorderElements();
+      if(is_array($obj->fb_fieldsToRender)) {
+        $iFields = array_intersect($info,$obj->fb_fieldsToRender);
+      } else {
+        $iFields = $info;
+      }
+      $allLangs = array_diff($langs,array($this->getDefaultLang($obj)));
+      array_unshift($allLangs,$this->getDefaultLang($obj));
+      $langs = $allLangs;
+
+      foreach($iFields as $field) {
+        $fields = array();
+        foreach($langs as $lang) {
+          $completename = $obj->fb_elementNamePrefix.$field.$obj->fb_elementNamePostfix;
+
+          $elem = $form->getElement($completename.'_'.$lang);
+
+          $elem->setAttribute('rel',$completename);
+          if($lang == $this->getDefaultLang($obj)) {
+            $class='translatesource field_'.$lang;
+            $id = 'autotransid_'.$completename;
+          } else {
+            $class='autotranslate source_autotransid_'.$completename.' field_'.$lang;
+            $id = 'autotransid_'.$completename.'_'.$lang;
+          }
+          $elem->setAttribute('class',$elem->getAttribute('class').($elem->getAttribute('class')?' ':'').$class);
+          $elem->setAttribute('id',$id);
+          $label = $elem->getLabel();
+          if(is_array($label)) {
+            $sublabel = $label[0];
+          } else {
+            $sublabel = $label;
+          }
+          $elem->setLabel($sublabel.'('.$lang.')');
+          $fields[] =$elem;
+          $form->removeElement($completename.'_'.$lang);
+
+        }
+        if(!$form->elementExists('__submit__')) {
+          $form->addElement('group', $completename.'_group',$label,$fields,'');
+        } else {
+          $form->insertElementBefore(MyQuickForm::createElement('group', $completename.'_group',$label,$fields),'__submit__');
+        }
+        if(in_array($field,$fb->fieldsRequired) || ($elements[$field] & DB_DATAOBJECT_NOTNULL)) {
+            $form->addGroupRule($completename.'_group',$fb->requiredRuleMessage,'required',null,1);
+        }
+      }
+    }
+
+  public function before_save(&$values,$fb)
+  {
+    $obj = $fb->_do;
+    if($this->_grouped) {
+      $this->before_save_grouped($values,$fb);
+      return;
+    }
+
+    $info = $obj->_getPluginsDef();
+    $info = $info['i18n'];
+    $elements = $obj->_i18nfbs[T::getLocale()]->_reorderElements();
+    // To avoid duplicate saving of current lang record
+    $this->_dontSavei18n = true;
+    unset($obj->i18n_lang);
+    unset($obj->i18n_record_id);
+    unset($obj->i18n_id);
+    $obj->whereAdd();
+    if($obj->pk()) {
+      $db = $obj->getDatabaseConnection();
+      $obj->whereAdd($db->quoteIdentifier($obj->pkName()).' = '.$db->quote($obj->pk()));
+    }
+  }
+  public function after_save_grouped(&$values,$fb)
+  {
+    $obj = $fb->_do;
+    $elements = $obj->_i18nfbs[T::getLang()]->_reorderElements();
+    $info = $obj->_getPluginsDef();
+
+    foreach($info['i18n'] as $field) {
+      $completename = $obj->fb_elementNamePrefix.$field.$obj->fb_elementNamePostfix;
+
+      $fieldhasempty = false;
+      $nonempty=null;
+      foreach($this->getLocales($obj) as $lang) {
+        $values[$completename.'_'.$lang] = $values[$field.'_group'][$completename.'_'.$lang];
+        if(empty($values[$completename.'_'.$lang])) {
+            $fieldhasempty = true;
+        } else {
+            $nonempty = $values[$completename.'_'.$lang];
+        }
+      }
+      if($fieldhasempty && (in_array($field,$fb->fieldsRequired) || ($elements[$field] & DB_DATAOBJECT_NOTNULL))) {
+          foreach($this->getLocales($obj) as $lang) {
+
+              if(empty($values[$completename.'_'.$lang])) {
+                  $values[$completename.'_'.$lang] = $nonempty;
+              }
+          }
+      }
+      unset($values[$completename.'_group']);
+    }
+
+    // To avoid duplicate saving of current lang record
+    $this->_dontSavei18n = true;
+    unset($obj->i18n_lang);
+    unset($obj->i18n_record_id);
+    unset($obj->i18n_id);
+  }
+
+  /**
+   * Generates a FormBuilder instance for each language.
+   * @params DB_DataObject main record
+   * @param array languages for which to create the FormBuilders
+   */
+  protected function prepareTranslationRecords($obj,$langs)
+  {
+    $info = $obj->_getPluginsDef();
+    $info = $info['i18n'];
+    $out = array();
+    $tablename = $obj->tableName().'_i18n';
+    if(is_array($obj->fb_fieldsToRender)) {
+      $iFields = array_intersect($info,$obj->fb_fieldsToRender);
+    }
+    if(count($iFields) == 0) $iFields = $info;
+    foreach($langs as $lang) {
+      $t = DB_DataObject::factory($tablename);
+      $t->i18n_lang=$lang;
+      $t->i18n_record_id = $obj->pk();
+      if($obj->pk()) {
+        $t->find(true);
+      }
+      foreach(get_object_vars($obj) as $var=>$val) {
+        if(preg_match('`^fb_`',$var)) {
+          $t->$var = $val;
+        }
+      }
+
+      $t->fb_elementNamePostfix.='_'.$lang;
+      foreach($info as $field) {
+        if(!is_array($t->fb_fieldAttributes[$field])) {
+          $t->fb_fieldAttributes[$field].=($t->fb_fieldAttributes[$field]?' ':'').'lang="'.$lang.'"';
+        } else {
+          $t->fb_fieldAttributes[$field]['lang'] = $lang;
+        }
+      }
+      if(is_array($t->fb_preDefElements)) {
+        foreach($t->fb_preDefElements as $key=>$elem) {
+          $elem2 = clone($elem);
+          if(method_exists($elem2,'updateAttributes')) {
+            $elem2->updateAttributes(array('name'=>$elem2->getAttribute('name').'_'.$lang,'lang'=>$lang));
+          }
+          $t->fb_preDefElements[$key] = $elem2;
+
+
+        }
+      }
+      $t->fb_fieldsToRender = $iFields;
+      $t->fb_createSubmit = false;
+      $t->fb_addFormHeader = true;
+      $t->fb_formHeaderText = $lang;
+      $out[$lang] = $t;
+    }
+    return $out;
+  }
+
+  protected function getDefaultLang($obj)
+  {
+    return T::getLocale();
   }
 
   protected function getLocales($obj)
