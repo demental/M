fcf2b98c3e7717e70399fc144add5ca1303605f5
arnaud sellenet <arnodmental@gmail.com>
2015-03-24 11:37:08 +0100

    helper fonctions

--- Office/functions.php
+++ Office/functions.php
@@ -4,10 +4,18 @@ function can($action, $module, $record = null) {
   return User::getInstance('office')->getDBDO()->can($action, $module, $record);
 }
 
+function deny_unless($condition) {
+  if($condition) return true;
+  throw new SecurityException(__('error.unauthorized_action'));
+}
 function deny_unless_can($action, $module, $record = null)
 {
   if(can($action, $module, $record)) return;
 
   throw new SecurityException(__('error.cannot_do_action_on_that', array($action, ($record ? $record : __("modules.$module.frontname")))));
+}
 
+function js_say($message) {
+  $js = 'say('.json_encode($message).');';
+  Mtpl::addJSinline($js);
 }
